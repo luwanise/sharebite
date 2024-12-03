46d3a2d010b2afb2d2d3b0ef52f0e2869d251218
luwanise <praiseoladejo@gmail.com>
2024-12-03 12:33:40 +0100

    Add app theme colors

---
+++ assets/Colors.tsx
@@ -0,0 +1,13 @@
+export const Colors = {
+    primary_1: '#FF8C42',
+    primary_2: '#4CAF50',
+    
+    secondary_1: '#6B8E23',
+    secondary_2: '#FFDDC1',
+  
+    accent_1: '#1E88E5',
+    accent_2: '#FBC02D',
+  
+    background_1: '#F5F0E1',
+    background_2: '#FDFDFD'
+  }
