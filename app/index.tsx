80a2605ab402ae13ffad9702c77c8da311e2cd75
luwanise <praiseoladejo@gmail.com>
2024-12-03 17:29:54 +0100

    Create Welcome Screen and Update App Icon

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
+      <StatusBar translucent hidden />
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/welcome">Welcome</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+})
---
+++ app/welcome.tsx
@@ -0,0 +1,89 @@
+import { Colors } from "@/assets/Colors";
+import { Button, Image, Pressable, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function WelcomeScreen() {
+  return (
+    <View style={styles.container}>
+        <StatusBar translucent hidden />
+        <View style={styles.logoContainer}>
+            <Image
+                source={require("../assets/images/icon.png")}
+                style={styles.icon}
+            />
+            <Image
+                source={require("../assets/images/design-resources/top_right_design.png")}
+                style={styles.image}
+            />
+        </View>
+        <Image
+            source={require("../assets/images/design-resources/welcome_image.png")}
+            style={styles.welcomeImage}
+        />
+        <View style={styles.textContainer}>
+            <Text style={styles.header}>Share a Bite, Change a Life</Text>
+            <Text style={styles.text}>Connect surplus food with those who need it. Make a difference, one meal at a time.</Text>
+        </View>
+        <TouchableOpacity
+            style={styles.button}
+            onPress={() => {}}
+        >
+            <Text style={styles.buttonText}>Get Started</Text>
+        </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: Colors.background_1
+  },
+  logoContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  icon: {
+    marginStart: 20,
+    width: 100,
+    height: 100
+  },
+  image: {
+    width: 120,
+    height: 120
+  },
+  welcomeImage: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
+  },
+  textContainer: {
+    width: "80%",
+    alignItems: "center",
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  text: {
+    fontSize: 16,
+    marginVertical: 10,
+    textAlign: "center"
+  },
+  button: {
+    width: "70%",
+    marginBottom: 60,
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: Colors.primary_1,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+
+  }
+})
