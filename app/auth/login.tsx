f638c8785adab0abe1e2fbd049061359dabc6bfd
luwanise <praiseoladejo@gmail.com>
2024-12-05 01:25:05 +0100

    Implment login functionality

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,15 +1,72 @@
-import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, Image, ScrollView, StyleSheet, Text, ToastAndroid, TouchableOpacity, View } from "react-native";
 import { Colors } from "@/assets/Colors";
 import { CustomTextInput } from "@/components/CustomTextInput";
 import { useState } from "react";
 import { CustomOrSeparator } from "@/components/CustomOrSeparator";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 import IonIcons from "@expo/vector-icons/Ionicons";
 import { Dimens } from "@/assets/Dimens";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/firebaseConfig";
 
 export default function LoginScreen() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const validateEmail = () => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+    
+    const validatePassword = () => {
+        return password.length > 0;
+    };
+
+    const validateForm = () => {
+        if (!validateEmail()) {
+            setLoading(false);
+            ToastAndroid.show("Please enter a valid email", ToastAndroid.LONG);
+            return false;
+        }
+      
+        if (!validatePassword()) {
+            setLoading(false);
+            ToastAndroid.show("Please enter a password", ToastAndroid.LONG);
+            return false;
+        }
+      
+        return true;};
+
+    const signInUser = () => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+            // Signed in
+            setLoading(false);
+                const userId = userCredential.user.uid;
+                router.dismissAll();
+                router.replace({
+                    "pathname": "/home",
+                    "params": { "userId": userId }
+                });
+            })
+            .catch((error) => {
+                setLoading(false);
+                if (error.code === 'auth/invalid-credential') {
+                    ToastAndroid.show('Invalid email or password', ToastAndroid.LONG);
+                } else {
+                    ToastAndroid.show(`An error occurred: ${error.message}`, ToastAndroid.LONG);
+                }
+            });
+    }    
+
+    const handleSignIn = () => {
+        setLoading(true);
+        if (!validateForm()) {
+            return;
+        }
+        signInUser();
+    }
     
     return (
         <ScrollView contentContainerStyle={styles.container}>
@@ -38,7 +95,7 @@ export default function LoginScreen() {
             <Link href={"/auth/forgot-password/forgotPassword"} style={styles.forgotPassword}>Forgot Password?</Link>
             <TouchableOpacity
                 style={styles.button}
-                onPress={() => {}}
+                onPress={() => handleSignIn()}
             >
                 <Text style={styles.buttonText}>Login</Text>
             </TouchableOpacity>
@@ -50,7 +107,12 @@ export default function LoginScreen() {
             <TouchableOpacity style={styles.googleButton} onPress={() => {}}>
                 <IonIcons name="logo-google" size={24} color={Colors.primary_2} />
                 <Text style={styles.googleButtonText}>Sign Up with Google</Text>
-            </TouchableOpacity>
+            </TouchableOpacity>    
+            {loading && (
+                <View style={styles.loaderContainer}>
+                    <ActivityIndicator size="large" color={Colors.primary_2} />
+                </View>
+            )}
         </ScrollView>
     )
 }
@@ -133,5 +195,11 @@ const styles = StyleSheet.create({
         fontSize: 16,
         fontFamily: "Lato_400Regular",
         color: Colors.primary_2,
-    }
+    },
+    loaderContainer: {
+        ...StyleSheet.absoluteFillObject,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(255, 255, 255, 0.6)',
+    },
 })
