f391377c7c4d9c09625ee813f4f2c801f680fd46
luwanise <praiseoladejo2003@gmail.com>
2024-12-05 19:09:10 +0100

    Change New Donations Tab Bar Button to Floating Icon Button

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,36 +1,57 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { Tabs } from 'expo-router';
+import { router, Tabs, useNavigation } from 'expo-router';
+import { TouchableOpacity, StyleSheet } from 'react-native';
 
 export default function TabLayout() {
+  const navigation = useNavigation();
+
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <Ionicons size={28} name="planet" color={color} />,
-          headerShown: false,
-          tabBarShowLabel: false
-        }}
-      />
-      <Tabs.Screen
-        name="NewDonation"
-        options={{
-            title: 'New Donation',
-            tabBarIcon: ({ color }) => <Ionicons size={28} name="add-circle" color={color} />,
+    <>
+      <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Home',
+            tabBarIcon: ({ color }) => <Ionicons size={28} name="planet" color={color} />,
             headerShown: false,
-            tabBarShowLabel: false
-        }}
-    />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: 'User Profile',
-          tabBarIcon: ({ color }) => <Ionicons size={28} name="person" color={color} />,
-          headerShown: false,
-          tabBarShowLabel: false
-        }}
-      />
-    </Tabs>
+            tabBarShowLabel: false,
+          }}
+        />
+        <Tabs.Screen
+          name="NewDonation"
+          options={{
+            title: 'New Donation',
+            tabBarButton: () => null,
+            headerShown: false,
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: 'Profile',
+            tabBarIcon: ({ color }) => <Ionicons size={28} name="person" color={color} />,
+            headerShown: false,
+            tabBarShowLabel: false,
+          }}
+        />
+      </Tabs>
+
+      {/* Floating Action Button */}
+      <TouchableOpacity
+        style={styles.fab}
+        onPress={() => router.navigate("/(tabs)/NewDonation")}
+      >
+        <Ionicons name="add-circle" size={56} color="blue" />
+      </TouchableOpacity>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  fab: {
+    position: 'absolute',
+    bottom: 20,
+    alignSelf: 'center',
+    zIndex: 1,
+  },
+});
