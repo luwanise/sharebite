45580b11db4a3763a011fbaee7d20196f90f2dc2
luwanise <praiseoladejo2003@gmail.com>
2024-12-05 19:26:01 +0100

    Design Tab Bar Layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,13 @@
+import { Colors } from '@/assets/Colors';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { router, Tabs, useNavigation } from 'expo-router';
+import { router, Tabs } from 'expo-router';
 import { TouchableOpacity, StyleSheet } from 'react-native';
 
 export default function TabLayout() {
-  const navigation = useNavigation();
 
   return (
     <>
-      <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+      <Tabs screenOptions={{ tabBarActiveTintColor: Colors.primary_1, tabBarStyle: { backgroundColor: Colors.background_1}}}>
         <Tabs.Screen
           name="index"
           options={{
@@ -41,7 +41,7 @@ export default function TabLayout() {
         style={styles.fab}
         onPress={() => router.navigate("/(tabs)/NewDonation")}
       >
-        <Ionicons name="add-circle" size={56} color="blue" />
+        <Ionicons name="add-circle" size={56} color={Colors.primary_2} />
       </TouchableOpacity>
     </>
   );
