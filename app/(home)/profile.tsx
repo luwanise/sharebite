9c1adf4f1fc8adba4d97b1cfe1fba3507abb1651
luwanise <praiseoladejo2003@gmail.com>
2024-12-08 14:57:39 +0100

    Add logout button in profile screen

--- app/(home)/profile.tsx
+++ app/(home)/profile.tsx
@@ -1,7 +1,44 @@
-import { Text } from "react-native";
+import { Colors } from "@/assets/Colors";
+import { Dimens } from "@/assets/Dimens";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { router } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function ProfileScreen() {
     return (
-        <Text>Profile</Text>
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.logoutButton} onPress={() => {router.dismissTo("/auth/login")}}>
+                <View style={styles.logoutButtonContainer}>
+                    <Ionicons name="log-out-outline" size={24} color={Colors.background_2} />
+                    <Text style={styles.logoutButtonText}>Logout</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: Dimens.padding
+    },
+    logoutButton: {
+        width: "100%",
+        backgroundColor: "red",
+        padding: 10,
+        borderRadius: 5,
+        alignItems: "center"
+    },
+    logoutButtonContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 10,
+    },
+    logoutButtonText: {
+        color: "white",
+        fontSize: 20,
+        fontFamily: "Montserrat_700Bold",
+    }
+})
