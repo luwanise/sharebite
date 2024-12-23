a7703149d32c9efaca45ecf167f14690bf35d886
luwanise <praiseoladejo2003@gmail.com>
2024-12-23 23:27:57 +0100

    Add logout functionality and show toast for unimplemented features in ProfileScreen

--- app/(home)/profile.tsx
+++ app/(home)/profile.tsx
@@ -2,10 +2,21 @@ import { Colors } from "@/assets/Colors";
 import { Dimens } from "@/assets/Dimens";
 import { ProfileHeader } from "@/components/Profile/ProfileHeader";
 import { ProfileItem } from "@/components/Profile/ProfileItem";
+import { auth } from "@/firebaseConfig";
 import { router } from "expo-router";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, ToastAndroid, View } from "react-native";
 
 export default function ProfileScreen() {
+
+    const showNotImplementedToast = () => {
+        ToastAndroid.show("Feature not implemented", ToastAndroid.SHORT);
+    };
+
+    const logout = () => {
+        auth.signOut();
+        router.dismissTo("/auth/login")
+    }
+
     return (
         <View style={styles.container}>
             <ProfileHeader />
@@ -13,10 +24,10 @@ export default function ProfileScreen() {
                 <ProfileItem title="Edit Profile" icon="person-outline" onPress={() => router.push("/profile-pages/EditProfile")} />
                 <ProfileItem title="Donations given" icon="gift-outline" onPress={() => router.push("/profile-pages/DonationsGiven")} />
                 <ProfileItem title="Donations claimed" icon="fast-food-outline" onPress={() => router.push("/profile-pages/DonationsClaimed")} />
-                <ProfileItem title="Change Password" icon="key-outline" onPress={() => {}} />
-                <ProfileItem title="Notifications" icon="notifications-outline" onPress={() => {}} />
-                <ProfileItem title="Report a Problem" icon="bug-outline" onPress={() => {}} />
-                <ProfileItem title="Logout" icon="log-out-outline" onPress={() => {router.dismissTo("/auth/login")}} />
+                <ProfileItem title="Change Password" icon="key-outline" onPress={showNotImplementedToast} />
+                <ProfileItem title="Notifications" icon="notifications-outline" onPress={showNotImplementedToast} />
+                <ProfileItem title="Report a Problem" icon="bug-outline" onPress={showNotImplementedToast} />
+                <ProfileItem title="Logout" icon="log-out-outline" onPress={logout} />
             </View>
         </View>
     )
